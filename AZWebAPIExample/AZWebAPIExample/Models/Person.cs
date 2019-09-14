e997ec985b67b0509d0ab86cfaf2d5286987435a
Kumar1 <Hitendra.Kumar1@tesco.com>
2019-09-14 14:24:45 +0530

    added project files

---
+++ AZWebAPIExample/AZWebAPIExample/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AZWebAPIExample.Models
+{
+    [Table("Person",Schema ="dbo")]
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "PersonId")]
+        public int ID { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        [Display(Name = "PersonName")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "int")]
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+    }
+}
